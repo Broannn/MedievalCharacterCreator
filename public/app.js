63294d0e395d83b73ab4fb39556fe47fd0395653
Léo Winterhalter <leo.winterhalter@heig-vd.ch>
2024-02-21 14:05:24 +0100

    Two-column style redesign

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('race').value = character.race;
         document.getElementById('job').value = character.metier;
 
-        // Parse les compétences si elles sont dans une chaîne de caractères formatée comme "Force: 5, Agilité: 4, ..."
+        // Parse les compétences à chaque fin de virgule
         if (character.competences) {
           const stats = character.competences.split(', ').map(stat => stat.split(': ')[1]);
           document.getElementById('strength').value = stats[0];
@@ -18,5 +18,16 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       })
       .catch(error => console.error('Erreur lors de la génération du personnage:', error));
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const readOnlyFields = document.querySelectorAll('input[readonly]');
+
+  // enlever le focus de chaque champ
+  readOnlyFields.forEach(field => {
+    field.addEventListener('focus', (event) => {
+      event.target.blur(); // Enlève le focus du champ immédiatement
+    });
   });
 });
